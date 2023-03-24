0084117e9d74ed7b1174a0945fb44d83f1c22b5f
Jake Napay <jakemantesnapay@gmail.com>
2023-03-24 13:57:36 +0800

    Login added

---
+++ login.php
@@ -0,0 +1,28 @@
+<?php include 'includes/config.inc.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Libag</title>
+
+    <!-- Link for css -->
+    <link rel="stylesheet" href="assets/css/style.css?v=<?php echo time() ?>">
+    <link rel="stylesheet" href="assets/css/sidebar.css?v=<?php echo time() ?>">
+
+    <!-- Link for FontAwesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.3.0/css/all.min.css">
+
+</head>
+
+<body>
+    <div>
+        <main>
+            <input type="button" value="">
+        </main>
+    </div>
+</body>
+
+</html>
